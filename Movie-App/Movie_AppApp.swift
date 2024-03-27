4ed9789dd910ee16fa97aa87bc32b615f1c56f2f
Mohamed Ali <mohamed.ali@intercom.com.eg>
2024-03-27 11:20:51 +0200

    Initial Commit

---
+++ Movie-App/Movie_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_AppApp.swift
+//  Movie-App
+//
+//  Created by Mohamed Ali on 27/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Movie_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
